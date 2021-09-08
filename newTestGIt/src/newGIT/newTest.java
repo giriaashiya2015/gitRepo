533000971c0b39e25f499dca2e61e6cd259f65df
mande <mande@DESKTOP-2RB0AGB.fios-router.home>
2021-09-07 20:07:43 -0400

    Final Commit

--- newTestGIt/src/newGIT/newTest.java
+++ newTestGIt/src/newGIT/newTest.java
@@ -2,7 +2,7 @@ package newGIT;
 
 public class newTest {
 	public static void main(String[] args) {
-		System.out.println(This the weebhook push");
+		System.out.println("This the weebhook push");
 	
 	}
 
